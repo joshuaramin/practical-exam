274461f4279d67e27b5e8aeba8d60373f0a1d6ab
joshuaramin <raminjoshua05@gmail.com>
2024-05-11 23:41:06 +0800

    removed error handling

--- pages/api/create.ts
+++ pages/api/create.ts
@@ -7,14 +7,14 @@ export default async function handler(req: NextRequest, res: NextApiResponse) {
   const { title, description, priority, status, userID, tags, users }: any =
     req.body;
 
-  if (!title) res.status(400).json({ message: "Title is required" });
-  if (!description)
-    res.status(400).json({ message: "Description is required" });
-  if (!priority) res.status(400).json({ message: "Priority is required" });
-  if (!status) res.status(400).json({ message: "Status is required" });
-  if (!tags) res.status(400).json({ message: "Tag is required" });
-  if (users.length < 0)
-    res.status(400).json({ message: "You need at least to assign" });
+  // if (!title) res.status(400).json({ message: "Title is required" });
+  // if (!description)
+  //   res.status(400).json({ message: "Description is required" });
+  // if (!priority) res.status(400).json({ message: "Priority is required" });
+  // if (!status) res.status(400).json({ message: "Status is required" });
+  // if (!tags) res.status(400).json({ message: "Tag is required" });
+  // if (users.length < 0)
+  //   res.status(400).json({ message: "You need at least to assign" });
 
   const result = await prisma.task.create({
     data: {
@@ -27,6 +27,13 @@ export default async function handler(req: NextRequest, res: NextApiResponse) {
       User: {
         connect: {
           userID,
+        },
+      },
+      Assign: {
+        create: {
+          User: {
+            connect: users,
+          },
         },
       },
     },
